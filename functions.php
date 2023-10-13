2a5d986bcbdbea37a2969e21c970ceef881e44af
Will Cuthbert <cuthbertw1@nku.edu>
2023-10-13 14:23:29 -0400

    modified:   functions.php

--- functions.php
+++ functions.php
@@ -3,68 +3,80 @@ session_start();
 
 function countArraysInJsonFile($jsonFilePath) {
     // Read the JSON file into a string
-    $jsonString = file_get_contents($jsonFilePath);
-    
-    // Parse the JSON data
-    $jsonData = json_decode($jsonString, true); 
-
-    if ($jsonData === null) {
-        // JSON parsing failed
-        throw new Exception("Failed to parse JSON data.");
-    }
-	$arrayCount=0;
-	foreach($jsonData as $value){
-		$arrayCount+=1;
+	if (file_exists($jsonFilePath)){
+		$jsonString = file_get_contents($jsonFilePath);
 		
-	}
+		// Parse the JSON data
+		$jsonData = json_decode($jsonString, true); 
 
-    echo $arrayCount;
+		if ($jsonData === null) {
+			// JSON parsing failed
+			throw new Exception("Failed to parse JSON data.");
+		}
+		$arrayCount=0;
+		foreach($jsonData as $value){
+			$arrayCount+=1;
+			
+		}
+
+		echo $arrayCount;
+	}else{
+		echo "0";
+	}
 }
-
 function totalCalories($jsonFilePath){
-	$jsonString=file_get_contents($jsonFilePath);
-	$jsonData=json_decode($jsonString, true);
-	//print_r($jsonData);
-	$total=0;
-	foreach ($jsonData as $value){
-		//print_r($value);
+	if (file_exists($jsonFilePath)){
+		$jsonString=file_get_contents($jsonFilePath);
+		$jsonData=json_decode($jsonString, true);
+		//print_r($jsonData);
+		$total=0;
+		foreach ($jsonData as $value){
+			//print_r($value);
+			
+			$total=$total+intval($value["CaloriesBurned"]);
+			
 		
-		$total=$total+intval($value["CaloriesBurned"]);
-		
-	
-	}
-	echo $total;
-	
+		}
+		echo $total;
+		}else{
+		echo "0";
+		}			
 }
 
 function totalTimeSpent($jsonFilePath){
-	$jsonString=file_get_contents($jsonFilePath);
-	$jsonData=json_decode($jsonString, true);
-	//print_r($jsonData);
-	$total=0;
-	foreach ($jsonData as $value){
-		//print_r($value);
+	if (file_exists($jsonFilePath)){
+		$jsonString=file_get_contents($jsonFilePath);
+		$jsonData=json_decode($jsonString, true);
+		//print_r($jsonData);
+		$total=0;
+		foreach ($jsonData as $value){
+			//print_r($value);
+			
+			$total=$total+intval($value["TimeWorkedOut"]);
+			
 		
-		$total=$total+intval($value["TimeWorkedOut"]);
-		
-	
+		}
+		echo $total;
+	}else{
+		echo "0";
 	}
-	echo $total;
-	
 }
 
 function totalCaloriesGoal($jsonFilePath){
-	$jsonString=file_get_contents($jsonFilePath);
-	$jsonData=json_decode($jsonString, true);
-	//print_r($jsonData);
-	$total=0;
-	foreach ($jsonData as $value){
-		//print_r($value);
+	if (file_exists($jsonFilePath)){
+		$jsonString=file_get_contents($jsonFilePath);
+		$jsonData=json_decode($jsonString, true);
+		//print_r($jsonData);
+		$total=0;
+		foreach ($jsonData as $value){
+			//print_r($value);
+			
+			$total=$total+intval($value["CalorieBurnGoal"]);
+			
 		
-		$total=$total+intval($value["CalorieBurnGoal"]);
-		
-	
-	}
-	echo $total;
-	
+		}
+		echo $total;
+	}else{
+		echo "0";
+	}		
 }
